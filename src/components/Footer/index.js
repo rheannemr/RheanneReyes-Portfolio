5d6480524650a368a304cf04032e4f92b040d0d0
Rheanne Reyes <rheannemr@gmail.com>
2021-04-11 22:29:55 -0700

    Add fontawesome icons + links to footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,10 +1,28 @@
 import React from "react";
 import "./style.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faFile } from "@fortawesome/free-solid-svg-icons";
+import { faInbox } from "@fortawesome/free-solid-svg-icons";
+
 
 function Footer() {
   return (
     <footer className="footer">
-      <span>Rheanne Reyes</span>
+      <a href="https://www.linkedin.com/in/rheanne-reyes-a8592a190/" rel="noreferrer" target="_blank">
+      <FontAwesomeIcon className="footer-icons" icon={faLinkedin} fixedWidth size="2x"/>
+      </a>
+      <a href="https://github.com/rheannemr" rel="noreferrer" target="_blank">
+      <FontAwesomeIcon className="footer-icons" icon={faGithub} fixedWidth size="2x"/>
+      </a>
+      <a href="../../assets/images/RRResume.png" target="_blank">
+      <FontAwesomeIcon className="footer-icons" icon={faFile} fixedWidth size="2x"/>
+      </a>
+      <a href="mailto: rheannemr@gmail.com" rel="noreferrer" target="_blank">
+      <FontAwesomeIcon className="footer-icons" icon={faInbox} fixedWidth size="2x"/>
+      </a>
+  
     </footer>
   );
 }
